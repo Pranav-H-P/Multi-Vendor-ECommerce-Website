b8a5883508b815ccb2565822b793d20773514ca9
Pranav <pranavhp871@gmail.com>
2024-12-02 23:00:04 +0530

    added proper popups for cart and profile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,8 +12,32 @@ export class HeaderComponent {
 
 
   userData = Inject(UserDataService);
-  
+
+  expandProfile: boolean = false;
+  expandCart: boolean = false;
+
   constructor(){
 
   }
+
+  toggleCart(event: MouseEvent){
+    event.stopPropagation();
+    console.log("clicked")
+    this.expandCart = !this.expandCart;
+    this.expandProfile = false;
+  }
+  toggleProfile(event: MouseEvent){
+    event.stopPropagation();
+    console.log("clicked")
+    this.expandProfile = !this.expandProfile;
+    this.expandCart = false;
+  }
+  closePopUps(){
+    console.log("close clicked")
+    this.expandCart = false;
+    this.expandProfile = false;
+    
+  }
+
+
 }
