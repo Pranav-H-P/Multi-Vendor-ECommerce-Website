601be7c28025bbe35d6db6aa9c0bf37ede43b902
Pranav <pranavhp871@gmail.com>
2024-12-03 00:43:42 +0530

    made the header partially dynamic, added cart, profile, search prefetch popup functionality, using mock data and no links now, should use db later

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,31 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject, Inject, signal } from '@angular/core';
 import { UserDataService } from '../services/user-data.service';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
 
-  userData = Inject(UserDataService);
+  searchTerm = "";
+  userData = inject(UserDataService);
+  apiService = inject(ApiService);
 
   expandProfile: boolean = false;
   expandCart: boolean = false;
+  expandSearch: boolean =false;
+
+
+  searchPreviews: string[] = [];
+  cartPreviews: any[][] = []; // make datatypes for this
+
+  cartSum = 0;
 
   constructor(){
 
@@ -22,21 +33,56 @@ export class HeaderComponent {
 
   toggleCart(event: MouseEvent){
     event.stopPropagation();
-    console.log("clicked")
+
+    if (!this.expandCart){ // to be opened
+      this.cartPreviews = this.apiService.getCart();
+
+      this.cartSum = 0;
+      this.cartPreviews.forEach(element => {
+        this.cartSum += element[1]*element[2];
+      });
+
+    }
     this.expandCart = !this.expandCart;
     this.expandProfile = false;
+    this.expandSearch = false;
   }
+
   toggleProfile(event: MouseEvent){
     event.stopPropagation();
-    console.log("clicked")
     this.expandProfile = !this.expandProfile;
     this.expandCart = false;
+    this.expandSearch = false;
   }
+
   closePopUps(){
-    console.log("close clicked")
     this.expandCart = false;
     this.expandProfile = false;
+    this.expandSearch = false;
     
+  }
+
+  search(event: MouseEvent){
+    event.stopPropagation();
+    this.expandCart = false;
+    this.expandProfile = false;
+  }
+
+  preFetch(){ // prefetches data and populates popup of search box
+    if (this.searchTerm.trim().length > 0){
+      this.searchPreviews = this.apiService.getSearchPreview(this.searchTerm.trim());
+      this.expandSearch = true;
+    }
+  }
+  openSearchBox(event: MouseEvent){
+    
+    event.stopPropagation();
+    this.expandCart = false;
+    this.expandProfile = false;
+
+    if (this.searchTerm.trim().length > 0){
+      this.expandSearch = true;
+    }
   }
 
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,10 +7,37 @@ export class ApiService {
 
   constructor() { }
 
+
+  dummyCartList = [
+    ["item1" , 100, 1],
+    ["dumbell", 400, 4],
+    ["item3", 50, 4]
+  ];
+  dummyItemList = ["item1", "testing", "item2", "barbell", "dumbell", "item3"];
+
   setJWT(token: string){
     localStorage.setItem('jwt', token);
   }
   getJWT(){
     const jwt = localStorage.getItem('jwt'); // stored just as a string
   }
+
+  getNSearchResults(itemName: string, n: number){
+    const matchingItems = this.dummyItemList.filter(item => 
+      item.toLowerCase().includes(itemName)
+    )
+    return matchingItems.slice(0, n);
+  }
+
+  getSearchPreview(itemName: string){
+
+    const lowerCaseItem = itemName.toLowerCase()
+    return this.getNSearchResults(lowerCaseItem, 5);
+
+  }
+
+  getCart(){
+    return this.dummyCartList;
+  }
+
 }
