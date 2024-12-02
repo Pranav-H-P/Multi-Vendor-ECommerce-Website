f833599a819c4259eb3cc00056eef3c8fd2e63d9
Pranav <pranavhp871@gmail.com>
2024-12-02 21:13:40 +0530

    basic responsiveness added to header, have to add functional parts like dropdown menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,26 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: { //default is extraextrasmall i suppose
+      
+      'extraSmall': '440px',
+
+      'small': '640px',
+      
+      'medium': '840px',
+
+      'large': '1040px',
+      
+
+      'extraLarge': '1240px',
+      
+    },
     extend: {
       fontFamily:{
         exo2: ['exo2'],
         bebas: ['bebasneue']
-      }
+      },
+      
     },
   },
   plugins: [],
